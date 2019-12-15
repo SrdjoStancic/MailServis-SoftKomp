264a4c1f3ca5e14de3247d8436e1eabe26b5ac9c
Srdjan Stancic <srdjo1906@gmail.com>
2019-12-15 17:02:17 +0100

    web app

---
+++ webAppZvanicni/probni_node_prvi567/public/js/baza.js
@@ -0,0 +1,60 @@
+let korisnici = [
+    {
+        email : "korisnik1@raf.rs",
+        pass : "1234",
+        name : "korisnik1",
+        admin: true
+    },
+    {
+        email : "korisnik2@raf.rs",
+        pass : "1234",
+        name : "drkorisnik2ugi",
+        admin: true
+
+    },
+    {
+        email : "korisnik3@raf.rs",
+        pass : "1234",
+        name : "korisnik3",
+        admin: false
+    },
+    {
+        email : "korisnik4@raf.rs",
+        pass : "1234",
+        name : "korisnik4",
+        admin: false
+    }
+];
+
+let korisnik = {
+    email: '',
+    name: '',
+    admin: false
+};
+
+let proizvodi = [
+    {
+        ime: 'IBANEZ JEM7VP STEVE VAI SIGNATURE IN WHITE ',
+        img: './Slicice/gitara.jpg',
+        gitara: 'Ibanez',
+        cena: 1700
+    },
+    {
+        ime: 'Fender Trzalice',
+        img: './Slicice/trzalica2.jpg',
+        gitara: 'Fender',
+        cena: 2
+    },
+    {
+        ime: 'Dunlop Zice za gitaru',
+        img: './Slicice/zice.jpg',
+        gitara: 'DunlopPro',
+        cena: 15
+    }
+   
+];
+
+
+let korpa = [
+    
+];
